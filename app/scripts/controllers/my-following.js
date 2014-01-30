8d9ddbfa198595f9f7e1d7daaf2def7c99b6f06d
Yun YoungSik <ysyun@yuwin.co.kr>
2014-01-29 21:13:41 -0500

    add contrllors in app.js for routing

--- app/scripts/controllers/my-following.js
+++ app/scripts/controllers/my-following.js
@@ -1,10 +1,8 @@
 'use strict';
 
 angular.module('meanstackApp')
-  .controller('MyFollowingCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MyFollowingCtrl', [
+  	'$scope',
+  	function ($scope) {
+    
+  }]);
